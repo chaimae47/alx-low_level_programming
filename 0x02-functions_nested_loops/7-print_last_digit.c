5d7eb508d620069435fdb267d1f271516765f303
chaimae47 <122215013+chaimae47@users.noreply.github.com>
2023-07-18 16:11:26 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
 * print_last_digit - Prints the last digit of a number.
-* @n: The number to extract the last digit from.
+* @n: The number.
 *
 * Return: The value of the last digit.
 */
@@ -17,5 +17,5 @@ last_digit = n % 10;
 
 _putchar('0' + last_digit);
 
-return last_digit;
+return (last_digit);
 }
