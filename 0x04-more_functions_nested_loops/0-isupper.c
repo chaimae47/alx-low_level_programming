37f011a107f2e59476c5ebb6734751d6864ac1f9
chaimae <chaimae2000fes@gmail.com>
2023-07-20 05:55:50 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 /**
- * _isupper - checks if a character is uppercase
- * @c: the character to be checked
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
+* _isupper - checks if a character is uppercase
+* @c: the character to be checked
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return 1;
-    else
-        return 0;
+if (c >= 'A' && c <= 'Z')
+return 1;
+else
+return (0);
 }
