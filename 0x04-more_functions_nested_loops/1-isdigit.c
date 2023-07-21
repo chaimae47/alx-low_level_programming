9de018ddf4ca0b83dc6ee3d42f67a4c1be66494d
chaimae <chaimae2000fes@gmail.com>
2023-07-21 04:07:35 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 /**
- * _isdigit - checks if a character is a digit
- * @c: the character to be checked
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
+* _isdigit - checks if a character is a digit
+* @c: the character to be checked
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-        return 1;
-    else
-        return 0;
+if (c >= '0' && c <= '9')
+return 1;
+else
+return (0);
 }
