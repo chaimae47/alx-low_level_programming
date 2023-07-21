dd9ae718d0edfd95d8b510d0906d99963551ce21
chaimae <chaimae2000fes@gmail.com>
2023-07-21 05:11:34 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,9 +4,10 @@
 #include <stdio.h>
 
 /* Function prototypes */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-
+void print_most_numbers(void);
 #endif /* MAIN_H */
