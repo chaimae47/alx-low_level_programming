166aaab6e3761ecceb668679137846e0dd983c1d
chaimae <chaimae2000fes@gmail.com>
2023-08-02 19:59:09 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,38 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - Returns the natural square root of a number .
+* _sqrt_recursive - Helper function to calculate the square root .
 *
 * @n: The number for which the square root is to be calculated.
-* Return: The natural square root of 'n', or -1 .
+* @start: The starting value for the search.
+* @end: The ending value for the search.
+* Return: The natural square root of 'n', or -1.
+*/
+int _sqrt_recursive(int n, int start, int end)
+{
+if (start > end)
+return (-1);
+
+int mid = (start + end) / 2;
+
+if (mid * mid == n)
+return (mid);
+
+if (mid * mid > n)
+return (_sqrt_recursive(n, start, mid - 1));
+
+
+return (_sqrt_recursive(n, mid + 1, end));
+}
+
+/**
+* _sqrt_recursion - Returns the natural square root of a number.
+*
+* @n: The number for which the square root is to be calculated.
+* Return: The natural square root of 'n', or -1.
 */
 int _sqrt_recursion(int n)
 {
-int i;
-
 if (n < 0)
 return (-1);
 
@@ -17,11 +40,5 @@ if (n == 0 || n == 1)
 return (n);
 
 
-for (i = 1; i * i <= n; i++)
-{
-if (i * i == n)
-return (i);
-}
-
-return (-1);
+return (_sqrt_recursive(n, 0, n));
 }
