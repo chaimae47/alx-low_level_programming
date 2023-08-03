a7897ae0c7ff6821bd8842956c178dcc120df99a
chaimae <chaimae2000fes@gmail.com>
2023-08-02 19:13:16 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 void _puts_recursion(char *s)
 {
 /*If the current character is '\0', we have reached the end of the string.*/
-if (*s == '\0') 
+if (*s == '\0')
 {
 _putchar('\n'); /* Print a new line and return the previous recursive call.*/
 return;
 }
 /* Print the current character.*/
-_putchar(*s); 
+_putchar(*s);
 /* Recursively call the function with the next character in the string.*/
-_puts_recursion(s + 1); 
+_puts_recursion(s + 1);
 }
