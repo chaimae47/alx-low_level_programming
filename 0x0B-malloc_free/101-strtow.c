4019e2207274650f5db4a17a247f7fd1a2f2f1fc
chaimae <chaimae2000fes@gmail.com>
2023-08-07 20:04:35 -0700

    malooc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,101 +1,107 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 /**
-* count_words - Counts the number of words in a string.
-* @str: The string to count words in.
+* count_words - Counts the number of words in a string
+* @str: The string to count
 *
-* Return: The number of words in the string.
+* Return: The number of words, or 0 if str is NULL or empty
 */
 int count_words(char *str)
 {
-int count = 0, in_word = 0;
+int i, count, in_word;
 
-for (int i = 0; str[i] != '\0'; i++)
+if (str == NULL || str[0] == '\0')
+return (0);
+
+in_word = 0;
+count = 0;
+for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] != ' ')
-{
-if (!in_word)
+if (str[i] == ' ')
+in_word = 0;
+else if (in_word == 0)
 {
 in_word = 1;
 count++;
 }
 }
-else
-{
-in_word = 0;
-}
-}
-
 return (count);
 }
 
 /**
-* strtow - Splits a string into words.
-* @str: The string to split.
+* copy_word - Copies a word from a string into a new string
+* @str: The source string
+* @start: The starting index of the word in the source string
+* @end: The ending index of the word in the source string
 *
-* Return: A pointer to an array of strings (words), or NULL on failure.
+* Return: A pointer to the new string, or NULL if it fails
+*/
+char *copy_word(char *str, int start, int end)
+{
+char *word;
+int i, j;
+
+word = malloc(sizeof(char) * (end - start + 2));
+if (word == NULL)
+return (NULL);
+
+j = 0;
+for (i = start; i <= end; i++)
+{
+word[j] = str[i];
+j++;
+}
+word[j] = '\0';
+return (word);
+}
+
+/**
+* strtow - Splits a string into words
+* @str: The string to split
+*
+* Return: A pointer to an array of strings (words),
+* or NULL if it fails
 */
 char **strtow(char *str)
 {
-if (str == NULL || str[0] == '\0')
+char **tab;
+int i, j, k, nwords, start, end;
+
+nwords = count_words(str);
+if (nwords == 0)
 return (NULL);
 
-int word_count = count_words(str);
-char **words = (char **)malloc((word_count + 1) * sizeof(char *));
-
-if (words == NULL)
+tab = malloc(sizeof(char *) * (nwords + 1));
+if (tab == NULL)
 return (NULL);
 
-int word_index = 0, word_length = 0;
-
-for (int i = 0; str[i] != '\0'; i++)
+j = 0;
+k = 0;
+for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] != ' ')
+if (str[i] != ' ' && j == 0)
 {
-word_length++;
-if (word_length == 1 || (i > 0 && str[i - 1] == ' '))
+start = i;
+j = 1;
+}
+if ((str[i] == ' ' || str[i + 1] == '\0') && j == 1)
 {
-words[word_index] = (char *)malloc(word_length * sizeof(char));
-if (words[word_index] == NULL)
+end = i;
+j = 0;
+tab[k] = copy_word(str, start, end);
+if (tab[k] == NULL)
 {
-for (int j = 0; j < word_index; j++)
-free(words[j]);
-free(words);
+while (k >= 0)
+{
+free(tab[k]);
+k--;
+}
+free(tab);
 return (NULL);
 }
-word_index++;
+k++;
 }
 }
-else
-{
-word_length = 0;
-}
-}
-
-word_index = 0;
-word_length = 0;
-
-for (int i = 0; str[i] != '\0'; i++)
-{
-if (str[i] != ' ')
-{
-words[word_index][word_length] = str[i];
-word_length++;
-if (word_length == 1 || (i > 0 && str[i - 1] == ' '))
-{
-words[word_index][word_length] = '\0';
-word_index++;
-}
-}
-else
-{
-word_length = 0;
-}
-}
-
-words[word_count] = NULL;
-
-return (words);
+tab[k] = NULL;
+return (tab);
 }
