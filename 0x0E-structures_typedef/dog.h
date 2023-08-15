9a748742937536c694d9d4bb392125785b88ca51
chaimae <chaimae2000fes@gmail.com>
2023-08-15 06:37:12 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}
+dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
+
 #endif /* DOG_H */
