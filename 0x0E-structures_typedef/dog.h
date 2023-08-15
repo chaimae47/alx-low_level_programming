7374e020925e5416ed7d706f1909b0966c17af12
chaimae <chaimae2000fes@gmail.com>
2023-08-15 06:42:08 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,8 +8,7 @@ float age;
 char *owner;
 }
 dog_t;
-
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
-
 #endif /* DOG_H */
