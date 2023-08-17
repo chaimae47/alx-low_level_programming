c5b9b6ae8bc0e3a423796dea837b1ebcca8a851f
chaimae <chaimae2000fes@gmail.com>
2023-08-17 08:29:56 -0700

    function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * This program prints the opcodes of its own main function.
+ *
+ * Usage: ./main number_of_bytes
+ * Output format:
+ * The opcodes are printed in hexadecimal, lowercase
+ * Each opcode is two characters long
+ * Listing ends with a new line
+ *
+ * You are allowed to use printf and atoi
+ * You have to use atoi to convert the argument to an int
+ *
+ * If the number of arguments is not correct,
+  print Error, followed by a new line, and exit with the status 1
+ * If the number of bytes is negative,
+  print Error, followed by a new line, and exit with the status 2
+ *
+ * You do not have to compile with any flags
+ *
+ * Note: if you want to translate your opcodes
+  to assembly instructions, you can use, for instance, udcli.
+ */
 int main(int argc, char *argv[])
 {
     int num_bytes;
@@ -9,7 +31,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Error\n");
-        return 1;
+        return (1);
     }
 
     num_bytes = atoi(argv[1]);
@@ -17,7 +39,7 @@ int main(int argc, char *argv[])
     if (num_bytes < 0)
     {
         printf("Error\n");
-        return 2;
+        return (2);
     }
 
     printf("%02x", *((unsigned char *)main));
@@ -28,5 +50,5 @@ int main(int argc, char *argv[])
     }
 
     printf("\n");
-    return 0;
+    return (0);
 }
