a43c9796977ccb9b951f2a9852c682ab229effa9
chaimae47 <122215013+chaimae47@users.noreply.github.com>
2023-08-30 23:47:48 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
-* _calloc - Allocates memory for an array
-* of nmemb elements of size bytes each.
-* @nmemb: Number of elements in the array.
-* @size: Size of each element in bytes.
+* _calloc - Allocates memory for an array and
+* initializes with zeros
+* @nmemb: Number of elements in the array
+* @size: Size of each element
 *
-* Return: Pointer to the allocated memory, or NULL on failure.
+* Return: A pointer to the allocated memory,
+* or NULL on failure
 */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
+void *_calloc(unsigned int nmemb, unsigned int size) {
 void *ptr;
 
 if (nmemb == 0 || size == 0)
@@ -19,10 +18,10 @@ return (NULL);
 
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
-return NULL;
+return (NULL);
 
-// Set memory to zero using memset
-memset(ptr, 0, nmemb * size);
+for (unsigned int i = 0; i < nmemb * size; i++)
+*((char *)ptr + i) = 0;
 
 return (ptr);
 }
