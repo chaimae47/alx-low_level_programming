46d2609ee316bcd5e2c8b9ae484e2ccd56bb9a50
chaimae47 <122215013+chaimae47@users.noreply.github.com>
2023-10-03 02:14:11 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,25 +6,35 @@
 * @n: The number for which the square root is to be calculated.
 * Return: The natural square root of 'n', or -1 .
 */
+
+/**
+* _sqrt_recursion - Returns the natural square
+* root of a number.
+* @n: The number for which to find the square root.
+* Return: The square root of n, or -1 if n doesn't have a
+* natural square root.
+*/
 int find_sqrt(int n, int guess);
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    else if (n == 0 || n == 1)
-        return (n);
-    else
-        return (find_sqrt(n, n / 2));
+if (n < 0)
+return (-1);
+else if (n == 0 || n == 1)
+return (n);
+else
+return (find_sqrt(n, n / 2, n));
 }
 
-int find_sqrt(int n, int guess)
+int find_sqrt(int n, int guess, int original_n)
 {
-    if (guess * guess == n)
-        return (guess);
-    else if (guess * guess < n)
-        return (find_sqrt(n, guess + 1));
-    else
-        return (find_sqrt(n, guess - 1));
-}
+if (guess <= 0)
+return (-1); // If the guess becomes non-positive, n doesn't have a natural square root.
 
+if (guess * guess == original_n)
+return (guess);
+else if (guess * guess < original_n)
+return (find_sqrt(original_n, guess + 1, original_n));
+else
+return (find_sqrt(original_n, guess - 1, original_n));
+}
