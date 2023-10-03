43f70905c53b7ce006d97e78cbb5c05bf8ee425e
chaimae47 <122215013+chaimae47@users.noreply.github.com>
2023-10-03 02:16:17 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,22 @@
-#include "main.h"
-
 /**
-* _sqrt_recursion - Returns the natural square root of a number .
+* find_sqrt - Helper function to recursively find the square root.
 *
-* @n: The number for which the square root is to be calculated.
-* Return: The natural square root of 'n', or -1 .
-*/
-
-/**
-* _sqrt_recursion - Returns the natural square
-* root of a number.
 * @n: The number for which to find the square root.
-* Return: The square root of n, or -1 if n doesn't have a
-* natural square root.
+* @guess: The current guess for the square root.
+*
+* Return: The square root of n, or -1 if n doesn't have
+* a natural square root.
 */
 int find_sqrt(int n, int guess);
 
+/**
+* _sqrt_recursion - Returns the natural square root of a number.
+*
+* @n: The number for which to find the square root.
+*
+* Return: The square root of n, or -1 if n doesn't have a
+* natural square root.
+*/
 int _sqrt_recursion(int n)
 {
 if (n < 0)
@@ -23,18 +24,24 @@ return (-1);
 else if (n == 0 || n == 1)
 return (n);
 else
-return (find_sqrt(n, n / 2, n));
+return (find_sqrt(n, n / 2));
 }
 
-int find_sqrt(int n, int guess, int original_n)
+/**
+* find_sqrt - Helper function to recursively find the square root.
+*
+* @n: The number for which to find the square root.
+* @guess: The current guess for the square root.
+*
+* Return: The square root of n, or -1 if n doesn't have a
+* natural square root.
+*/
+int find_sqrt(int n, int guess)
 {
-if (guess <= 0)
-return (-1); // If the guess becomes non-positive, n doesn't have a natural square root.
-
-if (guess * guess == original_n)
+if (guess * guess == n)
 return (guess);
-else if (guess * guess < original_n)
-return (find_sqrt(original_n, guess + 1, original_n));
+else if (guess * guess < n)
+return (find_sqrt(n, guess + 1));
 else
-return (find_sqrt(original_n, guess - 1, original_n));
+return (find_sqrt(n, guess - 1));
 }
