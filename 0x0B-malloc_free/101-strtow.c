4232fce70bfafdd9cfd2b31072c7002be6d7ec86
chaimae47 <122215013+chaimae47@users.noreply.github.com>
2023-10-04 03:21:15 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,92 +6,102 @@
 *
 * Return: The number of words, or 0 if str is NULL or empty
 */
-#include <stdlib.h>
-#include <stdio.h>
+int count_words(char *str)
+{
+int i, count, in_word;
 
-int countWords(const char *str) {
-int count = 0;
-int inWord = 0;
+if (str == NULL || str[0] == '\0')
+return (0);
 
-while (*str) {
-if (*str == ' ') {
-inWord = 0;
-} else {
-if (!inWord) {
+in_word = 0;
+count = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+in_word = 0;
+else if (in_word == 0)
+{
+in_word = 1;
 count++;
-inWord = 1;
 }
 }
-str++;
-}
-
 return (count);
 }
 
-char *strdupPartial(const char *start, const char *end) {
-int len = end - start;
-char *word = (char *)malloc(len + 1);
+/**
+* copy_word - Copies a word from a string into a new string
+* @str: The source string
+* @start: The starting index of the word in the source string
+* @end: The ending index of the word in the source string
+*
+* Return: A pointer to the new string, or NULL if it fails
+*/
+char *copy_word(char *str, int start, int end)
+{
+char *word;
+int i, j;
 
-if (word) {
-strncpy(word, start, len);
-word[len] = '\0';
+word = malloc(sizeof(char) * (end - start + 2));
+if (word == NULL)
+return (NULL);
+
+j = 0;
+for (i = start; i <= end; i++)
+{
+word[j] = str[i];
+j++;
 }
-
+word[j] = '\0';
 return (word);
 }
 
-char **strtow(char *str) {
-if (str == NULL || *str == '\0') 
-return (NULL);
-
-int numWords = countWords(str);
-char **result = (char **)malloc((numWords + 1) * sizeof(char *));
-
-if (result == NULL) 
-return (NULL);
-
-int inWord = 0;
-int wordIndex = 0;
-char *wordStart = str;
-
-while (*str) {
-if (*str == ' ') {
-if (inWord) {
-char *word = strdupPartial(wordStart, str);
-if (word) result[wordIndex++] = word;
-inWord = 0;
-}
-} else {
-if (!inWord) {
-wordStart = str;
-inWord = 1;
-}
-}
-str++;
-}
-
-if (inWord) {
-char *word = strdupPartial(wordStart, str);
-if (word) result[wordIndex++] = word;
-}
-
-result[wordIndex] = NULL;
-
-return (result);
-}
-
-int main(void) {
+/**
+* strtow - Splits a string into words
+* @str: The string to split
+*
+* Return: A pointer to an array of strings (words),
+* or NULL if it fails
+*/
+char **strtow(char *str)
+{
 char **tab;
-tab = strtow("      ALX School         #cisfun      ");
+int i, j, k, nwords, start, end;
 
-if (tab == NULL) 
-return (1);
+nwords = count_words(str);
+if (nwords == 0)
+return (NULL);
 
-for (int i = 0; tab[i] != NULL; ++i) {
-printf("%s\n", tab[i]);
-free(tab[i]);
+tab = malloc(sizeof(char *) * (nwords + 1));
+if (tab == NULL)
+return (NULL);
+
+j = 0;
+k = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && j == 0)
+{
+start = i;
+j = 1;
 }
-
+if ((str[i] == ' ' || str[i + 1] == '\0') && j == 1)
+{
+end = i;
+j = 0;
+tab[k] = copy_word(str, start, end);
+if (tab[k] == NULL)
+{
+while (k >= 0)
+{
+free(tab[k]);
+k--;
+}
 free(tab);
-return (0);
+return (NULL);
+}
+k++;
+}
+}
+tab[k] = NULL;
+return (tab);
 }
