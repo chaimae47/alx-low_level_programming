8dd5ca9d1037086dc0ed74ba6922de288af0ecbd
chaimae47 <122215013+chaimae47@users.noreply.github.com>
2023-10-05 01:45:43 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files into object files and create the static library liball.a
 gcc -c *.c
 ar rc liball.a *.o
-
-# Index the library for faster access (optional, but recommended)
 ranlib liball.a
-
-# Clean up object files (optional)
-rm *.o
